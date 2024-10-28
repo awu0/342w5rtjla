6f9070107d981e6b4e23b531543c564e03c85efc
awu0 <82164146+awu0@users.noreply.github.com>
2024-10-28 15:51:22 -0400

    colors for top bar

--- app/src/main/java/com/awu0/bookshelf/ui/BookshelfApp.kt
+++ app/src/main/java/com/awu0/bookshelf/ui/BookshelfApp.kt
@@ -8,10 +8,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,6 +61,10 @@ fun BookshelfTopBar(
                 style = MaterialTheme.typography.headlineSmall
             )
         },
-        modifier = modifier
+        modifier = modifier,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color(0xFF6200EE),
+            titleContentColor = Color.White,
+        )
     )
 }
